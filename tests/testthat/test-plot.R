852339c11bf270f1e9dba222f02b73734d67a1a8
AndrewC1998 <a.connell1@lancaster.ac.uk>
2019-06-04 12:27:54 +0100

    04/06/2019 This is the suggested fix by the vidffr package creator. AC

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -14,3 +14,4 @@ vdiffr::expect_doppelganger("Diagnostic plot (default)",
                             plot(obj.cpt.range, diagnostic = TRUE))
 vdiffr::expect_doppelganger("Diagnostic plot (histogram)",
                             plot(obj.cpt.range, diagnostic = TRUE, type = "h"))
+vdiffr::manage_cases()
