d9e0090376e736abdc7f25bab9fadbe788875296
AndrewC1998 <a.connell1@lancaster.ac.uk>
2019-06-05 10:11:06 +0100

    05/06/2019 Port of cpt.reg now in changepoint with front loading so EnvCpt now internally calls the functions in this package. cpt.reg is front facing so a user can input their own design matrix, a function has also been generated to do this for them. Description version has been updated but will jump again when these are pushed back to changepoint. AC

--- src/changepoint_init.c
+++ src/changepoint_init.c
@@ -1,19 +1,27 @@
 #include <stdlib.h> // for NULL
 #include <R_ext/Rdynload.h>
 
-/* FIXME: 
-   Check these declarations against the C/Fortran source code.
-*/
+/* FIXME:
+ Check these declarations against the C/Fortran source code.
+ */
 
 /* .C calls */
 extern void binseg(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void CptReg_Normal_AMOC(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void CptReg_Normal_PELT(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void Free_CptReg_Normal_AMOC(void *);
+extern void Free_CptReg_Normal_PELT(void *);
 extern void FreePELT(void *);
 extern void PELT(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
 
 static const R_CMethodDef CEntries[] = {
-    {"binseg",   (DL_FUNC) &binseg,   10},
-    {"FreePELT", (DL_FUNC) &FreePELT,  1},
-    {"PELT",     (DL_FUNC) &PELT,     11},
+    {"binseg",                  (DL_FUNC) &binseg,                  10},
+    {"CptReg_Normal_AMOC",      (DL_FUNC) &CptReg_Normal_AMOC,      13},
+    {"CptReg_Normal_PELT",      (DL_FUNC) &CptReg_Normal_PELT,      13},
+    {"Free_CptReg_Normal_AMOC", (DL_FUNC) &Free_CptReg_Normal_AMOC,  1},
+    {"Free_CptReg_Normal_PELT", (DL_FUNC) &Free_CptReg_Normal_PELT,  1},
+    {"FreePELT",                (DL_FUNC) &FreePELT,                 1},
+    {"PELT",                    (DL_FUNC) &PELT,                    11},
     {NULL, NULL, 0}
 };
 
